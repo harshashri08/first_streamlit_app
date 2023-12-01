0b2f339460eea3f940680f07ed06feaad79c445b
harshashri08 <152371977+harshashri08@users.noreply.github.com>
2023-12-01 07:25:29 +0000

    Update Second_streamlit_app.py

--- Second_streamlit_app.py
+++ Second_streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner /n Curd Rice')
+streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Dinner Menu')
+streamlit.text ('Gobimanchurian, Noodles & Palak Paneer')
+streamlit.text('Bluemoon spicy & Pshyc soda')
+streamlit.text('Curd Rice , Ghee Rice & Handi Biryani')
