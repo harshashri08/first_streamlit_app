0c35ffb4059c0ed8e786092250ba841b5ece8209
harshashri08 <152371977+harshashri08@users.noreply.github.com>
2023-12-01 07:06:11 +0000

    Update Second_streamlit_app.py

--- Second_streamlit_app.py
+++ Second_streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.description( 'Gobimanchurian and palak paneer' /n 'Veg pulav ,curd Rice and Rasam' )
+streamlit.description( 'Gobimanchurian and palak paneer')
