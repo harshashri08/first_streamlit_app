5e0ac3d7cfae9db91430e311d91a27761cd56bf2
harshashri08 <152371977+harshashri08@users.noreply.github.com>
2023-12-14 06:55:34 +0000

    Update Second_streamlit_app.py

--- Second_streamlit_app.py
+++ Second_streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('Bluemoon spicy 🍊 & Pshyc soda')
 streamlit.text('🍎Chicken Rice , 🍍Ghee Rice &🍚 Handi Biryani')
 
 import pandas
-my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
